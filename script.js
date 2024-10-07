2d3ed05dfaeaa49440345a596f75424297b544bf
Amar Mujkic <amarmujkic321@gmail.com>
2024-10-07 18:31:33 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function changeBackground() {
+    document.body.style.backgroundImage = "url('https://media.giphy.com/media/Vuw9m5wXviFIQ/giphy.gif')";
+    document.querySelector(".everything").style.display = "none"
+    document.getElementById("heart").style.display = "block";
+    document.getElementById("song").play()
+}
